1f021f117f68a0b8c8c99e6c63da2b4ebaa88f40
chanyir <161697788+chanyir@users.noreply.github.com>
2025-02-13 14:02:51 +0200

    Delete lessons directory

--- lessons/Lesson1/Demo2_Fundamentals/Demo4_Inputs.py
+++
@@ -1,2 +0,0 @@
-name = input("Enter Name\n")
-print(f"Hello {name}")
--- lessons/Lesson1/Demo3_Collections/Demo2_Tupple.py
+++
@@ -1,6 +0,0 @@
-col = (5,6,6,7,8)
-
-print(col[0] + 10)
-print(col)
-# TypeError: 'tuple' object does not support item assignment
-# col[0] = 20
